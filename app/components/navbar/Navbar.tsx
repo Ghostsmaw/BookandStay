33f2e002d1c9dc9f934360dc02b873072b49832e
ghostsmaw <ghostsmaw@gmail.com>
2023-08-14 04:03:42 +0100

    A startup idea for a hotel and apartment rental product

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,41 @@
+import { SafeUser } from "@/app/types";
+
+import Categories from "./Categories";
+import Container from "../Container";
+import Logo from "./Logo";
+import Search from "./Search";
+import UserMenu from "./UserMenu";
+
+interface NavbarProps {
+  currentUser?: SafeUser | null;
+}
+
+const Navbar: React.FC<NavbarProps> = ({
+  currentUser,
+}) => {
+  return ( 
+    <div className="fxed w-full bg-black z-10 shadow-sm">
+      <div
+        className="
+        py-0.1 border-b-[2px] border-[#355E3B]
+        "
+      >
+      <Container>
+        <div 
+          className="
+          flex flex-row items-center justify-between gap-3 md:gap-0
+          "
+        >
+          <Logo />
+          <Search />
+          <UserMenu currentUser={currentUser} />
+        </div>
+      </Container>
+    </div>
+    <Categories />
+  </div>
+  );
+}
+
+
+export default Navbar;
---
+++ app/components/navbar/UserMenu.tsx
@@ -0,0 +1,106 @@
+"use client";
+
+import { useCallback, useState } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+import { signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import useLoginModal from "@/app/hooks/useLoginModal";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+import useRentModal from "@/app/hooks/useRentModal";
+import { SafeUser } from "@/app/types";
+
+import MenuItem from "./MenuItem";
+import Avatar from "../Avatar";
+
+interface UserMenuProps {
+  currentUser?: SafeUser | null;
+}
+
+const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
+  const router = useRouter();
+
+  const loginModal = useLoginModal();
+  const registerModal = useRegisterModal();
+  const rentModal = useRentModal();
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value) => !value);
+  }, []);
+
+  const onRent = useCallback(() => {
+    if (!currentUser) {
+      return loginModal.onOpen();
+    }
+
+    rentModal.onOpen();
+  }, [loginModal, rentModal, currentUser]);
+
+  return (
+    <div className="relative">
+      <div className="flex flex-row items-center gap-3">
+        <div
+          onClick={onRent}
+          className="
+          hidden md:block text-sm text-white font-bold py-3 px-4 rounded-full hover:bg-neutral-900 transition cursor-pointer
+          "
+        >
+          LEASE YOUR HOME
+        </div>
+        <div
+          onClick={toggleOpen}
+          className="
+        p-4 md:py-2 md:px-2 border-[1.3px] border-[#355E3B] flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition
+          "
+        >
+          {/* <AiOutlineMenu /> */}
+          <div className="hidden md:block">
+            <Avatar src={currentUser?.image} />
+          </div>
+        </div>
+      </div>
+      {isOpen && (
+        <div
+          className="
+          absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-neutral-900 overflow-hidden right-0 top-12 text-sm
+          "
+        >
+          <div className="flex text-white flex-col cursor-pointer">
+            {currentUser ? (
+              <>
+                <MenuItem
+                  label="My trips"
+                  onClick={() => router.push("/trips")}
+                />
+                <MenuItem
+                  label="My favorites"
+                  onClick={() => router.push("/favorites")}
+                />
+                <MenuItem
+                  label="My reservations"
+                  onClick={() => router.push("/reservations")}
+                />
+                <MenuItem
+                  label="My properties"
+                  onClick={() => router.push("/properties")}
+                />
+                <MenuItem label="Lease your place" onClick={rentModal.onOpen} />
+                <hr />
+                <MenuItem label="Logout" onClick={() => signOut()} />
+              </>
+            ) : (
+              <>
+                <MenuItem label="Login" onClick={loginModal.onOpen} />
+                <MenuItem label="Sign up" onClick={registerModal.onOpen} />
+              </>
+            )}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UserMenu;
